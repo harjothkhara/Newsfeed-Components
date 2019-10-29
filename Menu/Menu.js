1c1b97ca8d5cb2dd1141f4c366a642c322206a39
harjoth <harjoth.khara@gmail.com>
2019-10-28 18:26:26 -0700

    netlify fixing

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,14 +1,16 @@
-
-//article flip 
-TweenMax.staggerFrom(".article", 1, {rotation:360, scale:1, delay:0.30}, 0.8) 
-
-
+//article flip
+TweenMax.staggerFrom(
+  '.article',
+  1,
+  { rotation: 360, scale: 1, delay: 0.3 },
+  0.8
+);
+// Toggle the "menu--open" class on your menu refence.
 const toggleMenu = () => {
-  // Toggle the "menu--open" class on your menu refence.
-  menu.classList.toggle('menu--open'); 
-  TweenMax.to(".menu", 0.50, { width: "190px", opacity: '1' }),
-    TweenMax.to(".menu ul li", 0.5, { right: 30, delay: 1.5 })
-}
+  menu.classList.toggle('menu--open');
+  TweenMax.to('.menu', 0.5, { width: '190px', opacity: '1' }),
+    TweenMax.to('.menu ul li', 0.5, { right: 30, delay: 1.5 });
+};
 
 // Start Here: Create a reference to the ".menu" class
 const menu = document.querySelector('.menu');
